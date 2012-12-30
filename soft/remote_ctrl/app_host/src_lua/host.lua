b4c45d7d8feaa53c947fb5c76cb9cd1e61d988a8
z80 <litedictteam@gmail.com>
2012-12-30 22:46:41 +0400

    Implemented MCU control routine.

--- soft/remote_ctrl/app_host/src_lua/host.lua
+++ soft/remote_ctrl/app_host/src_lua/host.lua
@@ -3,21 +3,24 @@ require( "luamcuctrl" )
 require( "bit" )
 
 local BOARDS_CNT = 3
-local ins = { 0, 0, 0 }
+local inputs = { 0, 0, 0 }
+local emulation = true
 
 function main()
-send( "print( \'host\'s entered main() dummy infinite loop\' )" )
-    while true do
-        sleep( 0.1 )
+    send( "print( \'host\'s entered main() dummy infinite loop\' )" )
+    if ( emulation ) then
+        emulationProcessMcu()
+    else
+        processMcu()
     end
 end
 
 function setValves( vals )
+    ins = {}
     local cnt = #vals
     for i = 1, cnt do
         ins[i] = vals[i] or 0
     end
-    remoteInvokeInputs( vals )
 end
 
 function remoteInvokeInputs( vals )
@@ -33,6 +36,49 @@ function remoteInvokeInputs( vals )
     end
     stri = stri .. "} ) )"
     send( stri )
+end
+
+function processMcu()
+    local mcu = luamcuctrl.create()
+    local en = mcu:open()
+    while true do
+        local ins = { mcu:inputs() }
+        if ( #ins < 3 ) then
+            -- Failure
+            send( "print( \'Error: mcu doesn\'t respond on USB requests\' )" )
+            mcu:close()
+            sleep( 5 )
+            cmu:open()
+        else
+            -- Success.
+            -- Compare current inputs with their new values from MCU.
+            for i = 1, BOARDS_CNT do
+                if ( inputs[i] ~= ins[i] ) then
+                    for i = 1, BOARDS_CNT do
+                        inputs[i] = ins[i]
+                    end
+                    remoteInvokeInputs( inputs )
+                    break
+                end
+            end
+        end
+        sleep( 0.1 )
+    end
+end
+
+function emulationProcessMcu()
+    while true do
+        for i = 1, BOARDS_CNT do
+            if ( inputs[i] ~= ins[i] ) then
+                for i = 1, BOARDS_CNT do
+                    inputs[i] = ins[i]
+                end
+                remoteInvokeInputs( inputs )
+                break
+            end
+        end
+        sleep( 0.1 )
+    end
 end
 
 function sleep( sec )
@@ -51,7 +97,7 @@ function image( w, h )
         imgProc:setPeer( peer() )
     end
     if ( imgProc:isRunning() ) then
-        send( "print( \"Pending image is in procress. Another image capture is possible only after finishing current one.\" )" )
+        send( "print( \"Pending image is in process. Another image capture is possible only after finishing current one.\" )" )
     else
         imgProc:start()
     end
