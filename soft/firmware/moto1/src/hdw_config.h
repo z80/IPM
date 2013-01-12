b5d5c06e0640fc9eefdfdc1870a671b1c6fc3c72
z80 <litedictteam@gmail.com>
2013-01-12 22:04:16 +0400

    Motor project definitions file started.

--- soft/firmware/moto1/src/hdw_config.h
+++ soft/firmware/moto1/src/hdw_config.h
@@ -2,38 +2,20 @@
 #ifndef __HDW_CONFIG_H_
 #define __HDW_CONFIG_H_
 
+// Inductive sensors.
+#define MAN_PORT        GPIOA
+#define MAN_INPUT_1     1
+#define MAN_INPUT_2     6
+
+#define SEN_PORT        GPIOC
+#define SEN_1           14
+#define SEN_2           15
+
 // LEDs
 #define LED_PORT   GPIOB
-#define LED_0_PIN  14
-#define LED_1_PIN  13
-#define LED_2_PIN  12
+#define LED_0_PIN  11
+#define LED_1_PIN  10
 
-// ADDRs - pins for identifying board index. It is defined by external physical switches.
-#define ADDR_PORT  GPIOC
-#define ADDR_0_PIN 15
-#define ADDR_1_PIN 14
-#define ADDR_2_PIN 13
-
-// OUTs
-#define OUT_PORT    GPIOA
-#define OUT_EN_PIN  4
-#define OUT_MR_PIN  6
-#define OUT_CP_PIN  5
-#define OUT_DSA_PIN 7
-
-// INs
-#define IN_PORT     GPIOA
-#define IN_PL_PIN   0
-#define IN_CE_PIN   1
-#define IN_CP_PIN   2
-#define IN_Q7_PIN   3
-
-// I2C
-#define I2C_BASE_ADDR  16
-#define I2C_SLAVES_CNT 2
-#define I2C_PORT      GPIOC
-#define I2C_SCL_PAD   6
-#define I2C_SDA_PAD   7
 
 
 #endif
