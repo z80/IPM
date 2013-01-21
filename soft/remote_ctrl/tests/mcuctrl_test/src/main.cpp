a0e547972b5c0e9496f4ce7da208cf9c97a615d1
z80 <litedictteam@gmail.com>
2013-01-21 09:26:12 +0400

    I2C io with moto_2 doesn't work. Status returned is 2. It means it doesn't accept transactions. Should be tried with fixed address first. After that it will be necessary to check I2C low level.

--- soft/remote_ctrl/tests/mcuctrl_test/src/main.cpp
+++ soft/remote_ctrl/tests/mcuctrl_test/src/main.cpp
@@ -7,52 +7,29 @@
 
 int main( int argc, char * argv[] )
 {
-    /*std::string text(" bababa ba {123 567 } { 12 45} ");
-
-    int adc1 = -1, adc2 = -1;
-    {
-        boost::regex patt( "\\{ {0,}\\d+" );
-        boost::sregex_iterator it( text.begin(), text.end(), patt );
-        boost::sregex_iterator end;
-        for ( ; it!=end; ++it )
-        {
-            std::cout << it->str() << "\n";
-            std::string stri = it->str().substr( 1 );
-            std::istringstream in( stri );
-            in >> adc1;
-            // v.push_back(it->str()); or something similar
-        }
-    }
-    {
-        boost::regex patt( "\\d+ {0,}\\}" );
-        boost::sregex_iterator it( text.begin(), text.end(), patt );
-        boost::sregex_iterator end;
-        for ( ; it!=end; ++it )
-        {
-            std::cout << it->str() << "\n";
-            std::string stri = it->str();
-            std::istringstream in( stri );
-            in >> adc2;
-            // v.push_back(it->str()); or something similar
-        }
-    }
-    std::cout << "adc1: " << adc1 << "\n";
-    std::cout << "adc2: " << adc2 << "\n";*/
-
     McuCtrl c;
     bool res = c.open();
     if ( !res )
         return 1;
 
-    //std::string stri;
-    //stri.resize( 128 );
-    //int cnt = c.read( stri );
-    //cnt = c.write( "mem\r\n" );
-    //cnt = c.read( stri );
-    //cnt = c.read( stri );
+    res = c.i2cSetAddr( 64 );
+    res = c.i2cSetAddr( 64 );
+    unsigned char data[6];
+    data[0] = 6;
+    data[1] = 1;
+    res = c.i2cSetBuf( 0, data, 2 );
+    int status = -1;
+    res = c.i2cStatus( status );
+    res = c.i2cStatus( status );
+    res = c.i2cStatus( status );
 
-    //bool r;
-    //r = c.powerConfig( 3, 3, 3 );
+    status = -1;
+    res = c.i2cIo( 2, 0 );
+    res = c.i2cStatus( status );
+    res = c.i2cIo( 2, 0 );
+    res = c.i2cStatus( status );
+    res = c.i2cIo( 2, 0 );
+    res = c.i2cStatus( status );
 
     return 0;
 }
