50778a0c7cdfef9fe430bf3d1687da4b80d330ac
z80 <bashkirov.sergey@gmail.com>
2013-04-18 10:55:24 +0400

    Made test firmware.

--- soft/firmware/joystick/src/main.c
+++ soft/firmware/joystick/src/main.c
@@ -5,16 +5,47 @@
 #include "ftdi_ctrl.h"
 #include "joystick_ctrl.h"
 
+#include "lpc2148.h"
+static void initTest( void );
+static void processTest( void );
+
 int main( void )
 {
     halInit();
     chSysInit();
 
-    initFtdi();
+    initTest();
+    //initFtdi();
     while ( TRUE )
     {
-        processShellFtdi(); 
-        chThdSleepMilliseconds( 500 );
+        processTest();
+        //processShellFtdi();
+        //chThdSleepMilliseconds( 500 );
     }
     return 0;
 }
+
+static void initTest( void )
+{
+    //#define P0_7     (0<<14)
+    //#define P0_7_MSK (3<<14)
+
+    // Set P0_7 as GPIO.
+    //PINSEL0 &= (~P0_7_MSK);
+    //PINSEL0 |= P0_7;
+    palSetPadMode( IOPORT1, PAL_PORT_BIT(7), PAL_MODE_OUTPUT_PUSHPULL );
+    palClearPad( IOPORT1, PAL_PORT_BIT(7) );
+}
+
+static void processTest( void )
+{
+    palClearPad( IOPORT1, PAL_PORT_BIT(7) );
+    chThdSleepMilliseconds( 500 );
+    palSetPad( IOPORT1, PAL_PORT_BIT(7) );
+    chThdSleepMilliseconds( 500 );
+
+}
+
+
+
+
